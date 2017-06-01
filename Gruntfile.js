8be899572cd682c8356b6781b5eeb9be30a34bcd
Jochen Stehle <joshsteele21@gmail.com>
2017-05-31 22:25:43 -0700

    run other grunt file to import from joomla

--- Gruntfile.js
+++ Gruntfile.js
@@ -70,7 +70,8 @@ module.exports = function(grunt) {
                     },
                 ]
             }
-        }
+        },
+
     });
 
     grunt.loadNpmTasks('grunt-contrib-compress');
@@ -78,27 +79,29 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-string-replace');
     grunt.loadNpmTasks('grunt-bumpup');
 
-    // grunt.loadNpmTasks('grunt-contrib-uglify');
-    // grunt.loadNpmTasks('grunt-contrib-cssmin');
-    // grunt.loadNpmTasks('grunt-spritesmith');
-    // grunt.loadNpmTasks('grunt-contrib-copy');
-    // grunt.loadNpmTasks('grunt-contrib-watch');
-    // grunt.loadNpmTasks('grunt-sass');
-    // grunt.loadNpmTasks('grunt-contrib-clean');
-    // grunt.loadNpmTasks('grunt-postcss');
-    // grunt.loadNpmTasks('grunt-contrib-concat');
-
+    /// ------------
 
     grunt.registerTask('default', ['compress']);
-    // grunt.registerTask('build_patch', ['sync:mod_tsvevents', 'version::patch', 'string-replace:version', 'compress']);
-    // grunt.registerTask('build_minor', ['sync:mod_tsvevents', 'version::minor', 'string-replace:version', 'compress']);
-    // grunt.registerTask('build_major', ['sync:mod_tsvevents', 'version::major', 'string-replace:version', 'compress']);
+    grunt.registerTask('import', ['run_grunt']);
     grunt.registerTask('build_patch', ['sync:mod_tsvevents', 'bumpup:patch', 'string-replace:version', 'compress']);
+    grunt.registerTask('build_minor', ['sync:mod_tsvevents', 'bumpup:minor', 'string-replace:version', 'compress']);
+    grunt.registerTask('build_major', ['sync:mod_tsvevents', 'bumpup:major', 'string-replace:version', 'compress']);
 
-    // grunt.registerTask('build', ['clean:build', 'concat:main', 'uglify:build', 'sass:build', 'postcss:dist', 'cssmin', 'copy:build', 'copy:buildmin']);
-    // grunt.registerTask('dev', ['clean:build', 'concat:main', 'sass:dev', 'postcss:dist', 'copy:dev']);
-    // grunt.registerTask('devplusmin', ['clean:build', 'concat:main', 'sass:dev', 'postcss:dist', 'copy:dev', 'uglify:build', 'sass:build', 'postcss:dist', 'cssmin', 'copy:buildmin']);
-    // grunt.registerTask('sprite-all', ['sprite:all']);
-    // grunt.registerTask('compsass', ['sass:dev', 'postcss:dist']);
+    // run grunt file in joomla folder
+    grunt.registerTask('run-grunt', function() {
+        var cb = this.async();
+        var child = grunt.util.spawn({
+            grunt: true,
+            args: ['build_mod_tsvevents'],
+            opts: {
+                cwd: 'D:\\xampp\\htdocs\\handball\\hb_joomla3\\'
+                }
+        }, function(error, result, code) {
+          cb();
+        });
+
+        child.stdout.pipe(process.stdout);
+        child.stderr.pipe(process.stderr);
+    });
 
 };
